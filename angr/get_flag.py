9360221d46b2299d8e88c94d13549b3879b4b750
Jayakrishna Menon <jkrshnmenon@gmail.com>
2017-01-13 20:13:19 +0530

    update angr scripts

--- angr/get_flag.py
+++ angr/get_flag.py
@@ -4,8 +4,9 @@ class get_flag:
 	def __init__(self,filename,load_opts):
 		self.p = angr.Project(filename,load_options = load_opts)
 		self.d = {'plt.printf':0,'plt.puts':0,'plt.write':0}
+		self.positives = ['Nice',' flag','Flag',' nice',' correct',' got it']
 		self.candidates = []
-		self.results = []
+		self.result = []
 		self.cfg = self.p.analyses.CFGAccurate()
 		for address,function in self.cfg.functions.iteritems():
 			if function.is_plt:
@@ -14,38 +15,62 @@ class get_flag:
 				continue
 			elif function.is_syscall or function.is_simprocedure:
 				continue
-			call_sites = function.get_call_sites()
-			if len(call_sites) <=0:
-				continue
-			if self.writes_to_stdout(call_sites,function) and self.references_flag(function):
+			if self.function_references_flag(function):
+				call_sites = self.writes_to_stdout(function)
 				for x in call_sites:
-					if x >= self.p.entry:
-						node = cfg.get_any_node(x)
-							self.candidates.append(node)
-		for x in self.candidates:
-			pg = self.p.factory.path_group()
-			pg.explore(find = x.addr)
-			for pp in pg.found:
-				self.results.append(pp)
-		return pp
+					if x >= self.p.entry and self.node_references_flag(x,function):
+						node = self.cfg.get_any_node(x)
+						self.candidates.append(node)
 
-	def writes_to_stdout(self,call_sites,function):
+		self.candidates.sort(key=lambda x: self.get_predecessor_length(x))
+
+	def writes_to_stdout(self,function):
+		call_sites = function.get_call_sites()
+		ret = []
+		if len(call_sites) <=0:
+			return False
 		for x in call_sites:
 			target = function.get_call_target(x)
 			for key in self.d.keys():
 				if self.d[key] == target:
-					return True
+					ret.append(x)
+		return ret
 
 	def get_chain_length(self,node):
-		
+		return 0
 	
 	def get_predecessor_length(self,node):
 		predecessor = node.predecessors
 		return len(predecessor)
 	
-	def references_flag(self,function):
+	def function_references_flag(self,function):
 		references = function.string_references()
 		for x in references:
-			if 'flag' in x[1]:
-				return True
+			for y in self.positives:
+				if y in x[1]:
+					return True
+	
+	def node_references_flag(self,block,function):
+		references = function.string_references()
+		block = self.p.factory.block(block)
+		for ins in block.capstone.insns:
+			if ins.insn.insn_name() == 'mov' and ins.insn.operands[1].type == 2:
+				target = ins.insn.operands[1].imm
+				for x in references:
+					if x[0] == target:
+						for y in self.positives:
+							if y in x[1]:
+								return True
+		return False
+				
+	
+	def get_candidates(self):
+		return self.candidates
 
+	def get_paths(self):
+		for x in self.candidates:
+			pg = self.p.factory.path_group()
+			pg.explore(find=x.addr)
+			for pp in pg.found:
+				self.result.append(pp)
+		return self.result
